23a6db9b03cfb2ac7e27dcd7810c9ea1a7d4e153
Hinata <69310436+inarin18@users.noreply.github.com>
2023-11-29 22:49:40 +0900

    First commit

---
+++ src/app.py
@@ -0,0 +1,23 @@
+#############################################
+# ToDo App
+# author : Hinata Inaoka
+##############################################
+
+import streamlit as st
+
+import os, json
+
+
+def config_session_state_vals():
+    pass
+
+
+def main():
+    
+    config_session_state_vals()
+    
+    st.title("ToDoApp")
+
+
+if __name__ == "__main__" :
+    main()
