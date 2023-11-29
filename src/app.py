8deef161b470a28968d354b7602e18034517b792
Hinata <69310436+inarin18@users.noreply.github.com>
2023-11-30 02:21:44 +0900

    タスクリストが空だった場合に警告をエラーメッセージを表示するように

--- src/app.py
+++ src/app.py
@@ -21,54 +21,61 @@ def main():
     initialize_session_state_vals()
     
     # タイトル
-    st.title("ToDoApp")
+    st.title(":green[ToDoApp]")
     
     # タスクリストの一覧表示
     st.subheader("タスクの一覧と編集", divider="rainbow")
-    df = pd.DataFrame(st.session_state.tasklist)
-    edited_df = st.data_editor(
-        df,
-        column_order=("name", "discription", "deadline", "is_checked"),
-        column_config={
-            "name" : st.column_config.Column(
-                "タスク名",
-                width="medium"
-            ),
-            "discription" : st.column_config.Column(
-                "タスクの説明",
-                width="medium"
-            ),
-            "deadline" : st.column_config.Column(
-                "締め切り",
-                width="medium"
-            ),
-            "is_checked" : st.column_config.CheckboxColumn(
-                "選択",
-                width="small",
-                disabled=False,
-                default=False,
-            )
-        },
-        hide_index=True
-    )
     
-    # 見栄えのためにボタンを配置する列を作成
-    col_delete, col_empty, col_edit = st.columns(3)
-    
-    with col_delete :
-        is_deleted = st.button("選択したタスクを削除")
-    with col_edit: 
-        is_edited = st.button("編集確定")
+    if st.session_state.tasklist == [] :
+        st.error("ToDoリストにタスクが追加されていません.")
+    else :
+        df = pd.DataFrame(st.session_state.tasklist)
+        edited_df = st.data_editor(
+            df,
+            column_order=("name", "discription", "deadline", "is_checked"),
+            column_config={
+                "name" : st.column_config.Column(
+                    "タスク名",
+                    width="medium"
+                ),
+                "discription" : st.column_config.Column(
+                    "タスクの説明",
+                    width="medium"
+                ),
+                "deadline" : st.column_config.Column(
+                    "締め切り",
+                    width="medium"
+                ),
+                "is_checked" : st.column_config.CheckboxColumn(
+                    "選択",
+                    width="small",
+                    disabled=False,
+                    default=False,
+                )
+            },
+            hide_index=True
+        )
         
-    # 選択したタスクを削除した場合は JSON ファイルを更新
-    if is_deleted :
-        delete_task_in_json_from_edited_df(edited_df)
-        st.warning("削除完了しました.")
+        # 見栄えのためにボタンを配置する列を作成
+        col_delete, col_reload, col_edit = st.columns(3)
         
-    # 編集された場合は JSON ファイルを更新
-    if is_edited:
-        update_taskjson_from_edited_df(edited_df)
-        st.info("編集完了しました.")
+        with col_delete : 
+            # 選択したタスクを削除した場合は JSON ファイルを更新
+            if st.button("選択したタスクを削除") :
+                delete_task_in_json_from_edited_df(edited_df)
+                st.warning("削除完了しました.")
+                
+        with col_reload:
+            # 更新されないときはボタンを押してリロード
+            if st.button("ページ更新"):
+                pass
+                st.info("更新しました")
+            
+        with col_edit: 
+            # 編集された場合は JSON ファイルを更新
+            if st.button("編集確定"):
+                update_taskjson_from_edited_df(edited_df)
+                st.info("編集完了しました.")
     
     # ----------------------------------------------------------------------------
     
@@ -84,17 +91,14 @@ def main():
         deadline    = st.date_input("タスクの期限")
         is_checked  = False # チェックボックス記入のために保持
         
-        # 追加ボタン
-        is_submitted = st.form_submit_button("追加")
-        
         # 追加ボタンが押された時の処理
-        if is_submitted:
+        if st.form_submit_button("追加"):
 
             task_body = {
                 "name"        : f"{task_name}",
                 "discription" : f"{discription}",
                 "deadline"    : f"{pd.to_datetime(deadline)}",
-                "is_checked"  : f"{is_checked}"
+                "is_checked"  : is_checked
             }
             
             # セッション変数中のタスクリストの最後尾にタスクを追加
@@ -102,7 +106,7 @@ def main():
             
             update_task_json()
             
-            st.info("タスクがToDoリストに追加されました.")    
+            st.info("タスクがToDoリストに追加されました.")
 
 
 if __name__ == "__main__" :
