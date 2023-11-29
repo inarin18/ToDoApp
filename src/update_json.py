b768a306ddb7cd5d1e6a6a98a7fe17926f6a9e85
Hinata <69310436+inarin18@users.noreply.github.com>
2023-11-30 02:49:26 +0900

    詳細なコメントを追加

--- src/update_json.py
+++ src/update_json.py
@@ -1,3 +1,10 @@
+#
+# json の操作に関する関数を集めたファイル
+# 
+# usage : from update_json import *
+# 
+
+
 import streamlit as st
 
 import os, json
@@ -13,6 +20,7 @@ __ALL__ = [
 TASK_JSON_PATH : os.path = '../data/tasklist.json'
 
 
+# jsonファイルをセッション変数内に保持している情報をもとに更新
 def update_task_json():
     
     # JSONファイルを更新するためにセッション変数をまず辞書に格納
@@ -26,12 +34,13 @@ def update_task_json():
         json.dump(task_dict, file, indent=2)
 
 
+# 編集された df をもとに json ファイルに含まれるタスクを削除
 def delete_task_in_json_from_edited_df(edited_df):
     edited_taskdict = edited_df.transpose().to_dict()
     edited_tasklist = [task for task in edited_taskdict.values()]
     
+    # チェックボックスの値を加味してタスクを新しいリストへ移す
     new_tasklist = []
-    
     for idx, edited_task in enumerate(edited_tasklist):
         if not edited_task["is_checked"] :
             new_tasklist.append(edited_tasklist[idx])
@@ -39,7 +48,8 @@ def delete_task_in_json_from_edited_df(edited_df):
     # 削除が完了したら明示的にチェックボックスの値を False に
     for task in new_tasklist :
         task["is_checked"] = False
-     
+    
+    # セッション変数を更新
     st.session_state.tasklist = new_tasklist
     
     update_task_json()
@@ -48,9 +58,11 @@ def delete_task_in_json_from_edited_df(edited_df):
 # 編集された df をもとにセッション変数内のタスクリストを更新
 def update_taskjson_from_edited_df(edited_df):
     
+    # df を辞書に変換したのちリストへ変換
     edited_taskdict = edited_df.transpose().to_dict()
     edited_tasklist = [task for task in edited_taskdict.values()]
     
+    # セッション変数を更新
     st.session_state.tasklist = edited_tasklist
     
     update_task_json()
